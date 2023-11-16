b564d8bf8b063cdb91e841d596cba7e15aae23b1
italofg <italo@agenciafg.com.br>
2023-11-16 12:15:02 -0300

    Alterações de Conteúdo SEO Coleções e categorias

---
+++ store-theme/react/components/TextoSeoColecao/searchDescriptionData.tsx
@@ -0,0 +1,11 @@
+export function SearchDescriptionData() {
+  const data = {
+    'obras-por-ambiente-cozinhas-e-banheiros': {
+      'bloco-text': [
+        '<h2></h2>'
+      ]
+    }
+  }
+
+  return data
+}
