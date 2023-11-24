e6d17086b1a2790ff25d92a00331d3e7f3d94bf4
italofg <italo@agenciafg.com.br>
2023-11-24 11:52:50 -0300

    ajustes de seo

--- store-theme/react/components/TextoSeoColecao/searchDescriptionData.tsx
+++ store-theme/react/components/TextoSeoColecao/searchDescriptionData.tsx
@@ -1,10 +1,56 @@
 export function SearchDescriptionData() {
   const data = {
-    'obras-por-ambiente-cozinhas-e-banheiros': {
+    'colecao-impermeabilizantes': {
       'bloco-text': [
-        '<h2></h2>'
+        '<h2>Impermeabilizantes em oferta, é só na FastObra! </h2>',
+        '<p>À procura de <strong>impermeabilizantes</strong> para a sua obra? Na FastObra você encontra os melhores produtos do mercado, fabricados por marcas líderes no setor. Essencial para proteger os ambientes, além de evitar a proliferação de fungos, mofo e umidade na sua casa, a impermeabilização dos ambientes é indispensável. </p>',
+        '<p>Afinal, além dos benefícios listados acima, alguns <strong>tipos de impermeabilizantes</strong> impedem o desgaste da pintura dos ambientes. Sem falar que o produto também evita infiltrações e assegura a integridade da estrutura!</p>',
+        '<h2>Como funcionam os impermeabilizantes? </h2>',
+        '<p>A <strong>tinta impermeabilizante</strong> é um produto de camada protetora que afasta fungos, mofo e umidade da sua casa. Sendo ideal para cuidados com a exposição climática, seja à vendavais ou tempestades.</p>',
+        '<p>Dessa forma, a aplicação de <strong>impermeabilizantes</strong> é essencial em todas as construções apoiadas sobre o solo. Principalmente naquelas com umidade entre seus grãos, vinda de lenções fráticos ou da própria chuva, por exemplo.</p>',
+        '<h2>Quais os tipos de impermeabilizantes?</h2>',
+        'Atualmente, é possível encontrar diferentes sistemas de impermeabilização. Entre eles, os impermeabilizantes rígidos e flexíveis. Para isso, você pode investir em aplicações bastante utilizadas, é possível adquirir produtos como: ',
+        '<h3>Argamassa polimérica </h3>',
+        '<p>Composta por cimento, polímeros, aditivos e agregados, a argamassa polimérica forma um revestimento impermeabilizante. Dessa forma, é indicada principalmente para locais úmidos e frios. Entre eles, piscinas, rodapés, reservatórios de água e subsolos. </p>',
+        '<h3>Emulsão asfáltica </h3>',
+        '<p>Por meio de mistura de água, material asfáltico e agentes dispersantes é formada a emulsão asfáltica. Sendo assim, geralmente, é aplicada no frio e indicada para elementos de fundação. Além de terraços, áreas frias e como <strong>impermeabilizante para laje.</strong> </p>',
+        '<h3>Manta de impermeabilização </h3>',
+        '<p>Indicada para vedação de lajes, a <strong>manta impermeabilizante</strong> pode ser encontrada em versões asfáltica, líquida e acrílica. É utilizada, principalmente, em locais onde há circulação de pessoas e com mais de 10 m².</p>',
+        '<h3>Aditivos impermeabilizantes </h3>',
+        '<p>Ideal para aplicação ainda durante a obra, os aditivos são impermeabilizantes de fundações, de tijolos e blocos e de argamassas. Em geral, são utilizados como <strong>impermeabilizante para parede</strong> ou em revestimentos de contrapisos em locais úmidos. </p>',
+        '<h2>Qual o melhor método de impermeabilização?</h2>',
+        '<p>Acima listamos os tipos de <strong>impermeabilizantes para piso</strong>, paredes e telhados mais conhecidos do mercado. No entanto, para saber qual a melhor opção para a sua obra, é preciso entender as necessidades da construção. </p>',
+        '<p>Para encontrar o melhor produto para a sua obra, nosso catálogo conta com variedade como a  <a href="https://www.fastobra.com.br/tinta-impermeabilizante-sika-igolflex-fachada--lata-20-kg----sika/p" target="_blank">Tinta Impermeabilizante Sika Igolflex Fachada (Lata 20 kg) - SIKA</a> e <a href="https://www.fastobra.com.br/tinta-impermeabilizante-vedapren-parede-branco--lata-18-kg----vedacit/p" target="_blank">Tinta Impermeabilizante Vedapren Parede Branco (Lata 18 kg) - VEDACIT</a>. </p>',
+        '<p>Sem falar no <a href="https://www.fastobra.com.br/impermeabilizante-viaplus-reparo--balde-12-kg----viapol/p" target="_blank">Impermeabilizante Viaplus Reparo (Balde 12 Kg) - VIAPOL</a> e <a href="https://www.fastobra.com.br/impermeabilizante-vedacit--balde-18-litros----vedacit/p" target="_blank">Impermeabilizante Vedacit (Balde 18 litros) - VEDACIT</a>. </p>',
+        '<h2>Compre impermeabilizantes na FastObra! </h2>',
+        '<p>Agora que você já conhece os benefícios de <strong>impermeabilizantes</strong> e porquê aplicá-lo em suas construções, é hora de adquirir o produto ideal! Na <a href="https://www.fastobra.com.br/" target="_blank">FastObra</a> é possível encontrar as melhores marcas do mercado com preços surpreendentes! </p>',
+        '<p>Aproveite as excelentes condições de pagamento, com parcelas em até 12x sem juros no cartão* e boas compras! </p>',
+        '<p>*Consulte condições!  </p>'
       ]
-    }
+    },
+    'colecao-mantas-asfalticas': {
+      'bloco-text': [
+        '<h2>Manta Asfáltica com preço imperdível, você encontra na FastObra! </h2>',
+        '<p>Essencial para a impermeabilização de telhados, a <strong>manta asfáltica</strong> é extremamente eficaz para o processo. Não à toa, é amplamente utilizada no Brasil. Afinal, é super versátil e composta por material que pode ser utilizado em projetos variados.  </p>',
+        '<p>Sendo assim, é um dos recursos mais usados quando falamos da impermeabilização de telhados, a <strong>manta asfáltica adesiva</strong> ou líquida está disponível na FastObra. Conheça! </p>',
+        '<h2>Para que serve a manta asfáltica? </h2>',
+        '<p>Bastante resistente e eficaz, a composição de <strong>mantas asfálticas para lajes</strong> também evita infiltrações e protege o local contra a umidade. Além disso, assegura contra danos na estrutura da obra. </p>',
+        '<p>Mas, em paralelo a isso, muitas pessoas se perguntam se o produto serve como isolante térmico. Temos a resposta! </p>',
+        '<p>Sim, a manta asfáltica também funciona como isolante térmico para os ambientes. Isso porque, para diminui a incidência do calor, a parte externa do alumínio refletirá os raios solares. Garantindo assim, que a temperatura não passe para dentro dos cômodos do local, fique retido na loja ou nas paredes da estrutura. </p>',
+        '<h2>Como instalar manta asfáltica? </h2>',
+        '<p>Para aplicar a <strong>manta asfáltica líquida</strong> ou adesiva, é essencial estar atento as indicações de uso das prescritas na embalagem do produto. Além disso, conte com a ajuda de profissionais especializados para que a impermeabilização fique perfeita!</p>',
+        '<p>Na aplicação da manta são necessários materiais como maçarico de alta pressão, espátula e luvas de proteção. Antes de começar, verifique se a superfície está limpa, seca e bem regularizada, contando com caimento para ralos e meia-cana nas quinas, que eliminam os cantos vivos. </p>',
+        '<p>Em seguida, aplique um primer e, quando o produto estiver seco, comece a fixar a <strong>manta asfáltica</strong>. Com o auxílio do maçarico cole a manta na base. Utilize a espátula para assentar a manta de forma que fique bem fixa, sem vãos por onde passa entrar água.</p>',
+        '<h2>Qual o preço da manta asfáltica?</h2>',
+        '<p>Na FastObra você encontra diversos modelos de mantas asfálticas com os melhores preços do mercado. </p>',
+        '<p>Entre eles a <a href="https://www.fastobra.com.br/manta-asfaltica-vedacit-pro-tipo-iii-b-3mm-poliester--rolo-1x10m----vedacit/p" target="_blank">Manta Asfáltica Vedacit PRO Tipo III-B 3mm Poliéster (Rolo 1x10m) - VEDACIT</a>, <a href="hhttps://www.fastobra.com.br/manta-asfaltica-vedacit-pro-tipo-iii-b-4mm-poliester--rolo-1x10m----vedacit/p" target="_blank">Manta Asfáltica Vedacit PRO Tipo III-B 4mm Poliéster (Rolo 1x10m) - VEDACIT</a> e  <a href="https://www.fastobra.com.br/manta-asfaltica-viapol-torodin-poliester-tipo-3-3mm--rolo-1-x10m----viapol/p" target="_blank">Manta Asfáltica Viapol Torodin Poliéster Tipo 3 3mm (rolo 1 x10m) - VIAPOL</a>. </p>',
+        '<p>Além do valor da <strong>manta asfáltica</strong> com excelente custo-benefício, em nossa loja on-line, você adquire acessórios essenciais para a instalação. Como o <a href="https://www.fastobra.com.br/macarico-manta-asfaltica-profissional-mt-01-completo---maxitorch/p" target="_blank"> Maçarico Manta Asfáltica Profissional MT-01 completo - Maxitorch</a>. </p>',
+        '<h2>Compre manta asfáltica com preço especial na FastObra!</h2>',
+        '<p>Adquira manta asfáltica na <a href="https://www.fastobra.com.br/" target="_blank">FastObra</a> com excelentes condições de pagamento e pague em até 12x sem juros*. Se preferir, adquira os melhores produtos para a sua obra também à vista via boleto bancário! </p>',
+        '<p>*Consulte condições!  </p>'
+        
+      ]
+    },
   }
 
   return data
